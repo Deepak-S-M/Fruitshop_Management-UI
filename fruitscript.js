27d424d1f5002188b77942a239066024e7ceb056
Deepak SM <103625162+Deepak-S-M@users.noreply.github.com>
2022-10-02 11:56:17 +0530

    fruitshop front end only updated

---
+++ fruitscript.js
@@ -0,0 +1,91 @@
+
+function init(){
+    var val1=document.getElementById("con-fruits");
+    val1.style.display='block';
+}
+    function conFruits()
+    {    
+        var val1=  document.getElementById("con-fruits");
+        var val2a=  document.getElementById("add-table");
+        var val3a=  document.getElementById("bill-table");
+        var val2b=  document.getElementById("con-min");
+        var val3b=  document.getElementById("con-bill"); 
+        var val4=  document.getElementById("con-all"); 
+        var val5=  document.getElementById("con-report");  
+        val2a.style.display='none';    
+        val3a.style.display='none';  
+        val2b.style.display='none';    
+        val3b.style.display='none';  
+        val4.style.display='none';  
+        val1.style.display='block';
+        val5.style.display='none';
+    }
+    function conAdd()
+    {  
+        var val1=  document.getElementById("con-fruits");
+        var val2a=  document.getElementById("add-table");
+        var val3a=  document.getElementById("bill-table");
+        var val2b=  document.getElementById("con-min");
+        var val3b=  document.getElementById("con-bill"); 
+        var val4=  document.getElementById("con-all"); 
+        var val5=  document.getElementById("con-report");  
+        val1.style.display='none';    
+        val3a.style.display='none'; 
+        val2b.style.display='block';    
+        val3b.style.display='none';  
+        val4.style.display='none';     
+        val2a.style.display='block';
+        val5.style.display='none';
+        
+    }
+    function conBill()
+    {   
+        var val1=  document.getElementById("con-fruits");
+        var val2a=  document.getElementById("add-table");
+        var val3a=  document.getElementById("bill-table");
+        var val2b=  document.getElementById("con-min");
+        var val3b=  document.getElementById("con-bill"); 
+        var val4=  document.getElementById("con-all"); 
+        var val5=  document.getElementById("con-report");  
+        val2a.style.display='none';    
+        val1.style.display='none';
+        val2b.style.display='none';
+        val4.style.display='none';      
+        val3b.style.display='block';      
+        val3a.style.display='block';
+        val5.style.display='none';
+    }
+    function conStack()
+    {   
+        var val1=  document.getElementById("con-fruits");
+        var val2a=  document.getElementById("add-table");
+        var val3a=  document.getElementById("bill-table");
+        var val2b=  document.getElementById("con-min");
+        var val3b=  document.getElementById("con-bill"); 
+        var val4=  document.getElementById("con-all"); 
+        var val5=  document.getElementById("con-report");  
+        val2a.style.display='none';    
+        val1.style.display='none';
+        val2b.style.display='none';
+        val4.style.display='block';      
+        val3b.style.display='none';      
+        val3a.style.display='none';
+        val5.style.display='none';
+    }
+    function conReport(){
+        var val1=  document.getElementById("con-fruits");
+        var val2a=  document.getElementById("add-table");
+        var val3a=  document.getElementById("bill-table");
+        var val2b=  document.getElementById("con-min");
+        var val3b=  document.getElementById("con-bill"); 
+        var val4=  document.getElementById("con-all");
+        var val5=  document.getElementById("con-report");  
+        val2a.style.display='none';    
+        val1.style.display='none';
+        val2b.style.display='none';
+        val4.style.display='none';      
+        val3b.style.display='none';      
+        val3a.style.display='none';
+        val5.style.display='block';
+    }
+    
